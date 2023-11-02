4e5e95d6bfb21a6ce6f869d6aab5ec77a3d888f8
Esgagos <135175639+Esgagos@users.noreply.github.com>
2023-11-02 01:28:24 +0000

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -3,7 +3,9 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <div className="container-fluid p-0 has-bg-img"></div>
+      <div className="container">
+        <div className="container-fluid p-0 has-bg-img"></div>
+      </div>
     </div>
   );
 }
